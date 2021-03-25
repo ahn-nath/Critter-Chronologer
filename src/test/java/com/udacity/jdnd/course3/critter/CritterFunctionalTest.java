32cdab1dbd20c49cf4bbf078d0b0ed43246f3f29
ahn-nath <67247850+ahn-nath@users.noreply.github.com>
2021-03-25 12:34:35 -0400

    refactor: removed unused code

--- src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
+++ src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
@@ -235,11 +235,7 @@ public class CritterFunctionalTest {
         compareSchedules(sched1, scheds1c.get(0));
 
         //Owner of pet from schedule 2 will be in both schedules 2 and 3
-        List<ScheduleDTO> scheds2c = scheduleController.getScheduleForCustomer(userController.getOwnerByPet(sched2.getPetIds().get(0)).getId());
-      
-        // debug
-        System.out.println("SCHEDULE:" + scheds2c.get(0));
-        
+        List<ScheduleDTO> scheds2c = scheduleController.getScheduleForCustomer(userController.getOwnerByPet(sched2.getPetIds().get(0)).getId()); 
         compareSchedules(sched2, scheds2c.get(0));
         compareSchedules(sched3, scheds2c.get(1));
     }
